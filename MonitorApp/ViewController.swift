fac9e08e72f8d108b0b90178aea307693561a512
Roy Hu <boyroyh@gmail.com>
2019-04-09 14:29:34 +0800

    Initial commit.

---
+++ MonitorApp/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  MonitorApp
+//
+//  Created by Roy Hu on 2019/4/9.
+//  Copyright © 2019 hwacom. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
